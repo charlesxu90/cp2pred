09e90d2fa3cd59bfb2003363907c50808ffd0e64
Xiaopeng Xu <charlesxu90@gmail.com>
2023-08-01 19:15:06 +0300

    udpate config and sbatch script

--- script/test_run.sh
+++ script/test_run.sh
@@ -1,14 +1,19 @@
 #==== Pretrain ====#
 # HELM BPE Tokenizer
 # python -m dataset.bpe --config configs/helm_bpe.yaml --output_dir results/helm_bpe
-# torchrun --nproc_per_node=2 train_bert.py --config configs/train_helms_bpe_bert_test.yaml --output_dir results/train_helms_bpe_bert_test --debug
+# config_name='train_helms_bpe_bert_test'
 
 # SMILES BPE Tokenizer
 # python -m dataset.bpe --config configs/smi_bpe.yaml --output_dir results/smi_bpe
-torchrun --nproc_per_node=2 train_bert.py --config configs/train_smi_bpe_bert_test.yaml --output_dir results/train_smi_bpe_bert_test --debug
+# config_name='train_smi_bpe_bert_test'
 
 # HELM BERT
-# torchrun --nproc_per_node=2 train_bert.py --config configs/train_helms_bert_test.yaml --output_dir results/train_helms_bert_test --debug
+# config_name='train_helms_bert_test'
+
+# SMILES BERT
+config_name='train_smi_bert_test'
+
+torchrun --nproc_per_node=2 train_bert.py --config configs/${config_name}.yaml --output_dir results/${config_name} --debug
 
 #==== evaluate pretrain ====#
 # HELM BERT
