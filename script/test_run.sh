f35cf0eaddc03cfc82fa640399749c4b5ac5639f
Xiaopeng Xu <charlesxu90@gmail.com>
2023-08-01 10:56:16 +0300

    remove task finetune

--- script/test_run.sh
+++ script/test_run.sh
@@ -12,11 +12,3 @@ torchrun --nproc_per_node=2 train_bert.py --config configs/train_helms_bert_test
 # python eval_pretrain.py --config configs/train_helms_bert_test.yaml  --ckpt_dir results/train_helms_bert/ --clf xgb --model_type bpe
 
 # python eval_pretrain.py --config configs/train_helms_bert_test.yaml  --ckpt_dir results/train_helms_bert2/ --clf xgb --model_type bpe
-
-
-#==== task finetune ====#
-# HELM BERT
-# torchrun --nproc_per_node=2 task_finetune.py --config configs/finetune_helm_bert.yaml --output_dir results/finetune_helm_bert --debug --ckpt results/train_helms_bert/model_1_1.041.pt
-
-#==== evaluate finetune ====#
-# python eval_task_finetune.py --config configs/finetune_helm_bert.yaml --ckpt_dir results/finetune_helm_bert --model_type bpe
