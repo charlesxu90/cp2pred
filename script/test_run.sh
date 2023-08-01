bfb57f0ed1cc186ece9adf158da92a14362cd008
Xiaopeng Xu <charlesxu90@gmail.com>
2023-08-01 11:59:45 +0300

    support smiles bpe

--- script/test_run.sh
+++ script/test_run.sh
@@ -1,9 +1,14 @@
 #==== Pretrain ====#
 # HELM BPE Tokenizer
 # python -m dataset.bpe --config configs/helm_bpe.yaml --output_dir results/helm_bpe
+# torchrun --nproc_per_node=2 train_bert.py --config configs/train_helms_bpe_bert_test.yaml --output_dir results/train_helms_bpe_bert_test --debug
+
+# SMILES BPE Tokenizer
+# python -m dataset.bpe --config configs/smi_bpe.yaml --output_dir results/smi_bpe
+torchrun --nproc_per_node=2 train_bert.py --config configs/train_smi_bpe_bert_test.yaml --output_dir results/train_smi_bpe_bert_test --debug
 
 # HELM BERT
-torchrun --nproc_per_node=2 train_bert.py --config configs/train_helms_bert_test.yaml --output_dir results/train_helms_bert_test --debug
+# torchrun --nproc_per_node=2 train_bert.py --config configs/train_helms_bert_test.yaml --output_dir results/train_helms_bert_test --debug
 
 #==== evaluate pretrain ====#
 # HELM BERT
@@ -11,4 +16,3 @@ torchrun --nproc_per_node=2 train_bert.py --config configs/train_helms_bert_test
 
 # python eval_pretrain.py --config configs/train_helms_bert_test.yaml  --ckpt_dir results/train_helms_bert/ --clf xgb --model_type bpe
 
-# python eval_pretrain.py --config configs/train_helms_bert_test.yaml  --ckpt_dir results/train_helms_bert2/ --clf xgb --model_type bpe
