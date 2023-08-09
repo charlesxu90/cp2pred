8987bf6f0c6f677c1bc05d55549410eca291a921
Xiaopeng Xu <charlesxu90@gmail.com>
2023-08-09 23:06:29 +0300

    support basic ray tune

--- tune_hparams.py
+++ tune_hparams.py
@@ -11,14 +11,22 @@ from dataset.dataset import load_feat_data, TaskDataset
 
 from model.task_model import ConcatModel
 from model.task_trainer import TaskTrainer
-import nni
+from functools import partial
 
-def update_config_by_nni_params(config, params):
+import ray
+from ray import tune
+from ray.air import Checkpoint, session
+from ray.tune.schedulers import ASHAScheduler
+
+def update_config(config, params):
     config.train.learning_rate = params['lr']
     config.model.mlp_hid_size = ','.join([str(params[p]) for p in ['l1_dim', 'l2_dim', 'l3_dim', 'l4_dim']])
     return config
 
-def main(params, args, config):
+def main(config, args, base_config):
+
+    config = update_config(base_config, config)
+
     device = torch.device(config.train.device)
     seed = 42
     
@@ -39,7 +47,6 @@ def main(params, args, config):
     train_dataloader = DataLoader(train_set, batch_size=batch_size, num_workers=num_workers)
     test_dataloader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-    config = update_config_by_nni_params(config, params)
     
     model = ConcatModel(input_size=feat_size, device=device, **config.model)
     trainer = TaskTrainer(model, args.output_dir, **config.train)
@@ -48,8 +55,14 @@ def main(params, args, config):
     for epoch in range(config.train.max_epochs):
         trainer.train_epoch(epoch, model, train_dataloader)
         _, spearman = trainer.test_epoch(epoch, model, test_dataloader)
-        nni.report_intermediate_result(spearman)
-    nni.report_final_result(spearman)
+        # raw_model = model.module if hasattr(model, "module") else model
+
+        # checkpoint_data = {
+        #     "epoch": epoch,
+        #     "model_state_dict": raw_model.state_dict(),
+        # }
+        # checkpoint = Checkpoint.from_dict(checkpoint_data)
+        session.report({"spearman": spearman}) #, checkpoint=checkpoint,)
 
 
 if __name__ == '__main__':
@@ -57,16 +70,39 @@ if __name__ == '__main__':
     parser.add_argument('--config', default='configs/train_conatmodel.yaml')
     parser.add_argument('--output_dir', default='results/train_conatmodel/')
     args = parser.parse_args()
-    config = parse_config(args.config)
+    base_config = parse_config(args.config)
+    
+    # ray.init()
+    # ray.init(address='127.0.0.1', _node_ip_address='127.0.0.1')
 
-    params = {
-        'l1_dim': 512,
-        'l2_dim': 512,
-        'l3_dim': 512,
-        'l4_dim': 512,
-        'lr': 0.001,
+    search_space_config = {
+        "l1_dim": tune.choice([2**i for i in range(3, 10)]),
+        "l2_dim": tune.choice([2**i for i in range(3, 10)]),
+        "l3_dim": tune.choice([2**i for i in range(3, 10)]),
+        "l4_dim": tune.choice([2**i for i in range(2, 8)]),
+        "lr": tune.loguniform(1e-7, 1e-1),
     }
-    optimized_params = nni.get_next_parameter()
-    params.update(optimized_params)
 
-    main(params, args, config)
+    scheduler = ASHAScheduler(
+        max_t=base_config.train.max_epochs,
+        grace_period=1,
+        reduction_factor=2)
+    
+    tuner = tune.Tuner(
+        tune.with_resources(
+            tune.with_parameters(partial(main, args=args, base_config=base_config)),
+            resources={"cpu": 2, "gpu": 1}
+        ),
+        tune_config=tune.TuneConfig(
+            metric="spearman",
+            mode="max",
+            scheduler=scheduler,
+            num_samples=100,
+        ),
+        param_space=search_space_config,
+    )
+
+    results = tuner.fit()
+
+    best_result = results.get_best_result("spearman", "max", "last")
+    print(f"Best trial config: {best_result.config}")
